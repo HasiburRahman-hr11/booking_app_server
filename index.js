a62865ea81c68a7ef74764ab5d0e071771e6cafb
hasiburRahman47@gmail.com <mdhasiburrahman47@gmail.com>
2022-08-29 15:56:12 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+// Using necessery middleware functions
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+app.use(cors());
+
+// Routes
+
+// PORT
+const PORT = process.env.PORT || 8000;
+
+const connectMongoDb = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB, {});
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+mongoose.connection.on('connected' , ()=>{
+    console.log('DB Connected');
+})
+mongoose.connection.on('disconnected' , ()=>{
+    console.log('DB Disconnected');
+})
+
+app.listen(PORT, () => {
+  connectMongoDb();
+  console.log(`Server is running at http://localhost:${PORT}`);
+});

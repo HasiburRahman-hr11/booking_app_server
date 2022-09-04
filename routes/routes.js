2bbc1bbede5bed68b20e0c4b9ee36ffd91113c53
hasiburRahman47@gmail.com <mdhasiburrahman47@gmail.com>
2022-09-04 12:59:58 +0600

    jwt

--- routes/routes.js
+++ routes/routes.js
@@ -1,13 +1,9 @@
-import auth from '../routes/auth.js';
+
 import users from '../routes/users.js';
 import rooms from '../routes/rooms.js';
 import hotels from '../routes/hotels.js';
 
 const routes = [
-    {
-        handler: auth,
-        path: '/api/auth'
-    },
     {
         handler: users,
         path: '/api/users'
